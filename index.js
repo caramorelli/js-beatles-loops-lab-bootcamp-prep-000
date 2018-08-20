e1da3746908db189da203feae3798c34ff18429c
Cara Morelli <caramorelli7@gmail.com>
2018-08-20 01:15:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@
 function theBeatlesPlay(musicians, instruments) {
   var beatles = [];
   for (var i = 0; i < musicians.length; i++) {
-    beatles.push(musicians[i] + " plays " + instruments[i])
+    beatles.push(musicians[i] + " plays " + instruments[i]);
   }
   return beatles;
-  
+ 
 }
 
